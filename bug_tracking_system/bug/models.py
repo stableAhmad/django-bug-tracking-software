d8d42ac35f714c9bc7b314c772445b35b7609eaa
ahmedabdelsalam <ahmedfw2178@gmail.com>
2022-09-03 09:37:46 +0200

    bug model without 2 field and migrated and added some apps into settings .py

--- bug_tracking_system/bug/models.py
+++ bug_tracking_system/bug/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+class bug(models.Model):
+	description = models.TextField(null = False , blank = False)
+	#project  foreign key , later
+	publisher = models.CharField(null = False , blank =False, max_length = 30)
+	date = models.DateField()
+	severity_CHOICES = ["critical", "normal",  "mild"]
+	severity = forms.ChoiceField(choices = severity_CHOICES)
+	assigned_to  = models.CharField( max_length = 30)
+	closed = models.BooleanField()
+	#attachment file field , later
+
--- bug_tracking_system/bug_tracking_system/settings.py
+++ bug_tracking_system/bug_tracking_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bug',
+    'project'
 ]
 
 MIDDLEWARE = [
