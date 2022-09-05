ba7ef17aab21eece27298960a31d87f31ebdfeaa
ahmedabdelsalam <ahmedfw2178@gmail.com>
2022-09-05 10:11:04 +0200

    configured static files path in settings.py

--- bug_tracking_system/bug_tracking_system/settings.py
+++ bug_tracking_system/bug_tracking_system/settings.py
@@ -120,9 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR , 'static/')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+   
+]
+
