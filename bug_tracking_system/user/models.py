ebec0cd37a6f6e8e6824f77605ec54d8e6c67b5f
ahmedabdelsalam <ahmedfw2178@gmail.com>
2022-09-14 01:32:34 +0200

    sign up wokring now 14/9 1.32 am

--- bug_tracking_system/user/models.py
+++ bug_tracking_system/user/models.py
@@ -8,6 +8,6 @@ class user(models.Model):
     lastname = models.CharField(null=False, blank=False, max_length=20)
     mail = models.EmailField(null=False,blank=False)
     password = models.CharField(null=False, blank=False , max_length=20)
-    teamleadermail = models.ForeignKey(team , null=True,on_delete = models.SET_NULL)
+    team = models.ForeignKey(team , blank = True, null=True,on_delete = models.SET_NULL)
     bugsreported = models.IntegerField()
     bugsclosed = models.IntegerField()
--- bug_tracking_system/user/views.py
+++ bug_tracking_system/user/views.py
@@ -12,50 +12,34 @@ def signed_up(request):
         if (DIC.get("first_name") != '' and DIC.get("last_name") != '' and DIC.get("pass") != '' and DIC.get(
                 "mail") != '' and DIC.get("team_leader_mail") != ''):
             mail = DIC["mail"]
-            col = user.objects.all()
-            unique = True
-            for x in col:
-                if mail == x.mail:
+            query = user.objects.filter(mail = DIC["mail"])
+            
+            if(query):
+                    
                     messDic["message"] = "E-mail is already used"
+                    
+            else :
+                    newUser = user(firstname=DIC["first_name"], lastname=DIC["last_name"], mail=DIC["mail"],
+                                password=DIC["pass"],  bugsreported=0,
+                                bugsclosed=0)
+                    newUser.save()
+                    messDic["signed"] = True
+                    messDic["message"] = "You can sign in now"
 
-                    unique = False
-                    break
-            if unique:
-                newUser = user(firstname=DIC["first_name"], lastname=DIC["last_name"], mail=DIC["mail"],
-                               password=DIC["pass"], teamleadermail=null, bugsreported=0,
-                               bugsclosed=0)
+
+            leader = DIC["team_leader_mail"]
+            q = team.objects.filter(teamleadermail = leader)
+            if(q):
+                newUser.team = q[0]
                 newUser.save()
-                messDic["signed"] = True
-                messDic["message"] = "You can sign in now"
-
-                leader = DIC["team_leader_mail"]
                 
-                # start here
-                #foundTL = False
-                #teamdb = team.objects.all()
 
-                #for it in teamdb:
-                    #if leader == it.teamleader:
-                        #it.teammembers.append(mail)
-                        #foundTL = True
-
-                #if not foundTL:
-                    #newTL = team(teamleadermail=DIC["team_leader_mail"])
-                    #newTL.teammembers.append(DIC["mail"])
-                   #newTL.save()
-
-        # search about mail
-        # tasks
-        # 1-search if the e-mail exists then not valid sign up cause user already exists
-        # 2-if e-mail doesnt exists we will create new user then ..
-        # we will search the team db for the team leader
-        # if found then we add a new memebr to the team of that leader
-        # if not we create new team in team db with that team leader
-        # then we add its that user as its first memebr
-        else:
-            messDic["message"] = "Please sign up with a valid input"
-            print("no valid input")
-
+            else :
+                newTeam = team(teamleadermail = leader)
+                newTeam.save()
+                newUser.team = newTeam
+                newUser.save()
+        
     return render(request, "signup.html", messDic)
 
 
