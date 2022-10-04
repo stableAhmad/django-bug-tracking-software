0d36b8afd4caedfe84c5b2da7349d8cd7f066ff4
Marwan Ayman <marwanayman249@gmail.com>
2022-10-04 19:29:55 +0200

    added reports database

--- bug_tracking_system/bug_tracking_system/settings.py
+++ bug_tracking_system/bug_tracking_system/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bug',
+    'report',
     'project',
     'team',
     'user'
