ca59c64af156210ddf477c422d391256b00a9a0b
ahmedabdelsalam <ahmedfw2178@gmail.com>
2022-10-05 20:57:24 +0200

    bjdlkajwdlkajwldk

--- bug_tracking_system/report/views.py
+++ bug_tracking_system/report/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import report
+from project.models import project
 
 
 def render_reports(request , id):
 	results = report.objects.all().filter(belongs_to__id = id)
-	context = {"reports":results}
+	current_project = project.objects.all().filter(id = id)[0]
+	context = {"project":current_project , "reports":results}
 	return render(request , "project.html",context)
