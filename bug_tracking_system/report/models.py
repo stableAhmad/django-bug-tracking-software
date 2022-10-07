69bffd130cd4094de0db4cbae47a861d6031c756
Marwan Ayman <92817153+Savoura@users.noreply.github.com>
2022-10-07 08:42:56 +0200

    finished reports page (#4)
    * finished reports page
    
    * edited project name in reports page
    
    * fixed the reports count in projects page

--- bug_tracking_system/report/models.py
+++ bug_tracking_system/report/models.py
@@ -1,42 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import User
 from team.models import team
-from project.models  import project
+from project.models import project
 from user.views import user_to_json
 from user.views import many_to_many_to_json
+
 
 class report(models.Model):
     title = models.CharField(max_length=30)
     options = (
-          ("S", "severe")
-        , ("n", "normal")
-        , ("L", "mild")
+        ("Severe", "severe")
+        , ("Normal", "normal")
+        , ("Mild", "mild")
     )
-    severity = models.CharField(max_length=20, choices=options, default="high")
+    severity = models.CharField(max_length=20, choices=options, default="severe")
     description = models.TextField()
-    attachment = models.FileField(upload_to='static/uploads/' , blank = True)
+    attachment = models.FileField(upload_to='static/uploads/', blank=True)
     states = (
-          ("O", "open")
-        , ("C", "closed")
-        , ("R", "reopened")
+        ("Open", "open")
+        , ("Closed", "closed")
+        , ("Reopened", "reopened")
     )
-    
+
     state = models.CharField(max_length=20, choices=states, default="open")
     date_added = models.DateField()
-    reported_by = models.ForeignKey(User , related_name = "submitter", on_delete=models.SET_NULL, null=True, blank=True)
-    assigned_to = models.ManyToManyField(User , related_name = "assigne")
-    belongs_to = models.ForeignKey(project , on_delete = models.SET_NULL  , null = True , blank = True)
+    reported_by = models.ForeignKey(User, related_name="submitter", on_delete=models.SET_NULL, null=True, blank=True)
+    assigned_to = models.ManyToManyField(User, related_name="assign")
+    belongs_to = models.ForeignKey(project, on_delete=models.SET_NULL, null=True, blank=True)
 
     def to_json(self):
-      
-      return {
-        'title':self.title,
-        'severity':self.severity,
-        'description':self.description,
-        'attachment':self.attachment.url,
-        "state":self.state,
-        "date_added":str(self.date_added),
-        "reported_by":user_to_json(self.reported_by),
-        "assigned_to":many_to_many_to_json(self.assigned_to),
-        "belongs_to":self.belongs_to.to_json()
-    }
+        return {
+            'title': self.title,
+            'severity': self.severity,
+            'description': self.description,
+            'attachment': self.attachment.url,
+            "state": self.state,
+            "date_added": str(self.date_added),
+            "reported_by": user_to_json(self.reported_by),
+            "assigned_to": many_to_many_to_json(self.assigned_to),
+            "belongs_to": self.belongs_to.to_json()
+        }
--- bug_tracking_system/report/views.py
+++ bug_tracking_system/report/views.py
@@ -5,36 +5,36 @@ from project.models import project
 
 from django.http import JsonResponse
 
-
 import json
 
-def render_reports(request , id):
-	results = report.objects.all().filter(belongs_to__id = id)
-	current_project = project.objects.all().filter(id = id)[0]
-	
-	
-	
-	context = {"project":current_project , "reports":results}
 
+def render_reports(request, id):
+    results = report.objects.all().filter(belongs_to__id=id)
+    current_project = project.objects.all().filter(id=id)[0]
+    current_project.bugs_count = results.count()
+    context = {"project": current_project, "reports": results}
 
-	if(request.method == 'GET'and request.headers.get("ajax")=="true" and request.headers.get("data")=="reports"):
-		id = request.headers.get("reportid")
-		target_report = results.filter(id= id)[0].to_json()
-		report_json = json.dumps(target_report)
-		return JsonResponse(report_json , safe = False)
-		
-	return render(request , "project.html",context)
+    if request.method == 'GET' and request.headers.get("ajax") == "true" and request.headers.get("data") == "reports":
+        id = request.headers.get("reportid")
+        target_report = results.filter(id=id)[0].to_json()
+        report_json = json.dumps(target_report)
+        return JsonResponse(report_json, safe=False)
+
+    return render(request, "project.html", context)
+
 
 def get_final_json_response(list_of_objects):
-	json_list = []
-	for value in  list_of_objects:
-		json_list.append(json.dumps(value))
+    json_list = []
+    for value in list_of_objects:
+        json_list.append(json.dumps(value))
 
-	json_list = json.dumps(json_list)
-		
-	return JsonResponse(json_list , safe = False)
+    json_list = json.dumps(json_list)
+
+    return JsonResponse(json_list, safe=False)
 
 
 def all_reports(request):
-	
-	return render(request , "reports.html")	
+    reports = report.objects.all()
+    context = {"reports": reports}
+
+    return render(request, "reports.html", context)
